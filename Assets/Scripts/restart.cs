bba52a1d254d4d61369d8466678aa30238e4b156
Vortanex <aj8451664@gmail.com>
2025-07-18 00:03:11 +0500

    restart

---
+++ Assets/Scripts/restart.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restart : MonoBehaviour
+{
+    [SerializeField] private float delay = 1f; 
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            StartCoroutine(RestartSceneWithDelay());
+        }
+    }
+
+    private IEnumerator RestartSceneWithDelay()
+    {
+        yield return new WaitForSeconds(delay);
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
+    }
+}
+
